50ba1f5986ab8ca10ad0dde5354d60fc3e19bc91
toshusai <toshusai.net@gmail.com>
2023-08-26 12:00:26 +0900

    feat: add layout

---
+++ src/layout/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const RootDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: hidden;
+`;
+
+export const HeaderDiv = styled.div`
+  display: flex;
+  height: 16px;
+`;
+
+export const MainDiv = styled.div`
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const BottomSpaceDiv = styled.div`
+  height: 8px;
+`;
