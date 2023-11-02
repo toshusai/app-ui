74dea244ef5208c8c981e1f645db767bd4ffcadd
toshusai <toshusai.net@gmail.com>
2023-11-03 07:03:37 +0900

    refactor: mv SelectRect

--- src/styled/index.ts
+++ src/styled/index.ts
@@ -1,6 +1,5 @@
 export * from "./GlobalStyle";
 export * from "./IconButton";
-export * from "./SelectRect";
 export * from "./StyledCheckbox";
 export * from "./StyledSelect";
 export * from "./StyledTextarea";
